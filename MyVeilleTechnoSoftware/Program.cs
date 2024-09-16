6606c082ea31781c8e0f2438fb31f2adae99ca81
Thomas BUREAU DU COLOMBIER <sio.bureauducolombier@gmail.com>
2024-09-16 17:01:41 +0200

    Instructions pour le live prochain

--- MyVeilleTechnoSoftware/Program.cs
+++ MyVeilleTechnoSoftware/Program.cs
@@ -10,5 +10,10 @@ Console.WriteLine("Bienvenue dans votre application de gestion de veille technol
 //Récupérer tous les liens du fichier JSON
 List<LinkModel> allLinks = LinksRepository.GetAllLinks();
 
+//Permettre à l'utilisateur de rechecher sur le titre et sur la description
+
+
+//Permettre à l'utilisateur de créer un lien (et de l'ajouter au fichier JSON)
+
 //Proposer à l'utilisateur de choisir un lien à ouvrir
 LinksRepository.ProposeToUserToOpenLink(allLinks);
