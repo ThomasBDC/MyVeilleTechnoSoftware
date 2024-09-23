371004793fffd4d4c350a678cad87733ab00416e
Thomas BUREAU DU COLOMBIER <sio.bureauducolombier@gmail.com>
2024-09-23 16:27:18 +0200

    Fonctionnalité : Ajout de lien

--- MyVeilleTechnoSoftware/Program.cs
+++ MyVeilleTechnoSoftware/Program.cs
@@ -7,13 +7,27 @@ using MyVeilleTechnoSoftware.Repository;
 
 Console.WriteLine("Bienvenue dans votre application de gestion de veille technologique !");
 
-//Récupérer tous les liens du fichier JSON
-List<LinkModel> allLinks = LinksRepository.GetAllLinks();
+
+//Menu de l'application 
+Console.WriteLine("Que voulez-vous faire ? ");
+Console.WriteLine("1 - Consulter les liens");
+Console.WriteLine("2 - Créer un lien");
+
+var response = Console.ReadLine();
+
+switch (response)
+{
+    case "1":
+        //Récupérer tous les liens du fichier JSON
+        List<LinkModel> allLinks = LinksRepository.GetAllLinks();
+        //Proposer à l'utilisateur de choisir un lien à ouvrir
+        LinksRepository.ProposeToUserToOpenLink(allLinks);
+        break;
+    case "2":
+        //Permettre à l'utilisateur de créer un lien (et de l'ajouter au fichier JSON)
+        LinksRepository.CreateLinkScreen();
+        break;
+}
+
 
 //Permettre à l'utilisateur de rechecher sur le titre et sur la description
-
-
-//Permettre à l'utilisateur de créer un lien (et de l'ajouter au fichier JSON)
-
-//Proposer à l'utilisateur de choisir un lien à ouvrir
-LinksRepository.ProposeToUserToOpenLink(allLinks);
--- MyVeilleTechnoSoftware/Repository/LinksRepository.cs
+++ MyVeilleTechnoSoftware/Repository/LinksRepository.cs
@@ -1,17 +1,20 @@
 ﻿using MyVeilleTechnoSoftware.Models;
 using System.Text.Json;
 using MyVeilleTechnoSoftware.Utils;
+using System.Text.Json.Nodes;
+using System.Xml;
 
 namespace MyVeilleTechnoSoftware.Repository
 {
     public static class LinksRepository
     {
+        private static string pathLinksJson = "C:\\Users\\Thomas BDC\\source\\repos\\MyVeilleTechnoSoftware\\MyVeilleTechnoSoftware\\Data\\links.json";
         public static List<LinkModel> GetAllLinks()
         {
             List<LinkModel> allLinks = new List<LinkModel>();
 
             //Lire le fichier JSON, et le transformer en objet
-            using (StreamReader r = new StreamReader("Data/links.json"))
+            using (StreamReader r = new StreamReader(pathLinksJson))
             {
                 string json = r.ReadToEnd();
                 allLinks = JsonSerializer.Deserialize<List<LinkModel>>(json);
@@ -33,6 +36,45 @@ namespace MyVeilleTechnoSoftware.Repository
             int indexToOpen = 0;
             indexToOpen = int.Parse(response);
             UtilsTools.OpenBrowser(allLinks[indexToOpen].Url);
+        }
+
+        public static void CreateLinkScreen()
+        {
+            Console.WriteLine("Nom du lien :");
+            string nomLien = Console.ReadLine();
+
+            Console.WriteLine("Description du lien :");
+            string descriptionLien = Console.ReadLine();
+
+            Console.WriteLine("Url du lien :");
+            string urlLien = Console.ReadLine();
+
+            //Objet à créer dans mon JSON
+            var monLien = new LinkModel(Guid.NewGuid(), nomLien, urlLien, descriptionLien);
+
+            CreateLink(monLien);
+        }
+
+        public static void CreateLink(LinkModel model)
+        {
+            //Je récupère tous mes liens
+            var allLinks = GetAllLinks();
+
+            //J'ajoute le nouveau lien à ma liste
+            allLinks.Add(model);
+
+            //allLinks -> JSON
+            string updatedJson = JsonSerializer.Serialize(allLinks);
+
+            try
+            {
+                //JSON -> Ecraser le fichier
+                File.WriteAllText(pathLinksJson, updatedJson);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
